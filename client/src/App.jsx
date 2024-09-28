e68f1fa46ef5f0e4606d912e240fb95da00266e8
Brandon Bagby <brandonbagby@Brandons-MacBook-Pro.local>
2024-09-28 16:28:57 -0400

    Added Friends UI and styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Friends from "./pages/Friends";
 import Header from "./components/Header";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/friends' element={<Friends />} />
       </Routes>
     </BrowserRouter>
   );
