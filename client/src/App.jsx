a39b8f2f6868cfa48f5f33b6458adf2dce643415
Brandon Bagby <brandonbagby@Brandons-MacBook-Pro.local>
2024-10-01 13:44:54 -0400

    Updating UI to match rest of App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Friends from "./pages/Friends"
+import Friends from "./pages/Friends";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
@@ -23,9 +23,9 @@ export default function App() {
         <Route path='/about' element={<About />} />
         <Route path='/signin' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
-        <Route path='/friends' element={<Friends />} />
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
+          <Route path='/friends' element={<Friends />} />
           <Route path="/fitness" element={<Fitness />}/>
           <Route path="/fitness/history" element={<FitnessHistory />}/>
           <Route path="/fitness/cardio" element={<Cardio />}/>
--- client/src/pages/Friends.jsx
+++ client/src/pages/Friends.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import "../styles/Friends.css";
 
 // TODO SWENG-50: Update events with calls to the database rather than using Mock Data
+// TODO SWENG-50: Move this page back to private route
 
 const Friends = () => {
   const navigate = useNavigate();
@@ -32,59 +32,74 @@ const Friends = () => {
   };
 
   return (
-    <div className="mainWrapper">
-      <div className="container1">
-        <div className={"titleContainer"}>
-          <div className="home-title">Add New Friend</div>
-        </div>
-        <div>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder="Search for a username"
-          />
-          <div className={"buttonContainer"}>
-            <button className="home-btn" onClick={handleSearch}>Search</button>
+    <div>
+      <div className="titleContainer">
+        <h2 className="title">Manage Friends List</h2>
+      </div>
+      <div className="friendWrapper">
+        <div className="friend-div">
+          <div className={"titleContainer"}>
+            <div className="home-title">Add New Friend</div>
+          </div>
+          <div>
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder="Search for a username"
+            />
+            <div className={"buttonContainer"}>
+              <button className="home-btn" onClick={handleSearch}>
+                Search
+              </button>
+            </div>
+          </div>
+          <div>
+            <ul
+              className={
+                searchResults.length > 0 ? "resultList" : "emptyResults"
+              }
+            >
+              {searchResults.map((name, index) => (
+                <li key={index}>
+                  <span className="friend">{name}</span>
+                  {!addedFriends.includes(name) && (
+                    <button
+                      className="add-btn"
+                      onClick={() => handleAddFriend(name)}
+                    >
+                      Add
+                    </button>
+                  )}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-        <div>
-          <ul
-            className={searchResults.length > 0 ? "resultList" : "emptyResults"}
-          >
-            {searchResults.map((name, index) => (
-              <li key={index}>
-                <span className="friend">{name}</span>
-                {!addedFriends.includes(name) && (
-                  <button className="add-btn" onClick={() => handleAddFriend(name)}>
-                    Add
-                  </button>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
 
-      <div className="container2">
-        <div className={"titleContainer"}>
-          <div className="home-title">Friends List</div>
-        </div>
-        <div>
-          <ul className="resultList">
-            {addedFriends.length === 0 ? (
-              <li>No friends added yet!</li>
-            ) : (
-              addedFriends.map((friend, index) => (
-                <li key={index}>
-                  <span className="friend">{friend}</span>
-                  <button className="small-delete-btn" onClick={() => handleRemoveFriend(friend)}>
-                    Remove
-                  </button>
-                </li>
-              ))
-            )}
-          </ul>
+        <div className="friend-div">
+          <div className={"titleContainer"}>
+            <div className="home-title">Friends List</div>
+          </div>
+          <div>
+            <ul className="resultList">
+              {addedFriends.length === 0 ? (
+                <li>No friends added yet!</li>
+              ) : (
+                addedFriends.map((friend, index) => (
+                  <li key={index}>
+                    <span className="friend">{friend}</span>
+                    <button
+                      className="small-delete-btn"
+                      onClick={() => handleRemoveFriend(friend)}
+                    >
+                      Remove
+                    </button>
+                  </li>
+                ))
+              )}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
